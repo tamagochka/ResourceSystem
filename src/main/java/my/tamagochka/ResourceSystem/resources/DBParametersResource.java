6eb7fd55098c51ba55fdeccce5d39724fba6c859
tamagochka <tamagochka@gmail.com>
2018-07-18 22:16:54 +0300

    Added DBParametersResource class and DBParametersResource.xml resource file.

---
+++ src/main/java/my/tamagochka/ResourceSystem/resources/DBParametersResource.java
@@ -0,0 +1,29 @@
+package my.tamagochka.ResourceSystem.resources;
+
+public class DBParametersResource {
+    private final String name;
+    private final String user;
+    private final String pass;
+
+    public DBParametersResource() {
+        name = "";
+        user = "";
+        pass = "";
+    }
+
+    public DBParametersResource(String name, String user, String pass) {
+        this.name = name;
+        this.user = user;
+        this.pass = pass;
+    }
+
+    public String getName() { return name; }
+    public String getUser() { return user; }
+    public String getPass() { return pass; }
+
+    @Override
+    public String toString() {
+        return "DBParametersResource{name='" + name + "', user='" + user + "', pass='" + pass + "'";
+    }
+
+}
